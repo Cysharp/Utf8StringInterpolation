f5eb8729ee1ee305499454ed4ee3ec066b76f241
hadashiA <dev@hadashikick.jp>
2023-12-08 19:21:37 +0900

    Remove PolySharp

--- src/Utf8StringInterpolation/Shims.cs
+++ src/Utf8StringInterpolation/Shims.cs
@@ -3,6 +3,57 @@
 using System.Buffers.Text;
 using System.Text;
 
+#if !NET6_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Indicates the attributed type is to be used as an interpolated string handler.
+    /// </summary>
+    [global::System.AttributeUsage(
+        global::System.AttributeTargets.Class |
+        global::System.AttributeTargets.Struct,
+        AllowMultiple = false, Inherited = false)]
+    [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    internal sealed class InterpolatedStringHandlerAttribute : global::System.Attribute
+    {
+    }
+    
+    /// <summary>
+    /// Indicates which arguments to a method involving an interpolated string handler should be passed to that handler.
+    /// </summary>
+    [global::System.AttributeUsage(global::System.AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    internal sealed class InterpolatedStringHandlerArgumentAttribute : global::System.Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="global::System.Runtime.CompilerServices.InterpolatedStringHandlerArgumentAttribute"/> class.
+        /// </summary>
+        /// <param name="argument">The name of the argument that should be passed to the handler.</param>
+        /// <remarks><see langword="null"/> may be used as the name of the receiver in an instance method.</remarks>
+        public InterpolatedStringHandlerArgumentAttribute(string argument)
+        {
+            Arguments = new string[] { argument };
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="global::System.Runtime.CompilerServices.InterpolatedStringHandlerArgumentAttribute"/> class.
+        /// </summary>
+        /// <param name="arguments">The names of the arguments that should be passed to the handler.</param>
+        /// <remarks><see langword="null"/> may be used as the name of the receiver in an instance method.</remarks>
+        public InterpolatedStringHandlerArgumentAttribute(params string[] arguments)
+        {
+            Arguments = arguments;
+        }
+
+        /// <summary>
+        /// Gets the names of the arguments that should be passed to the handler.
+        /// </summary>
+        /// <remarks><see langword="null"/> may be used as the name of the receiver in an instance method.</remarks>
+        public string[] Arguments { get; }
+    }    
+}
+#endif
+
 namespace Utf8StringInterpolation
 {
     internal static partial class Shims
